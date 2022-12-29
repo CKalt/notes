db58d78860bffdf976b7daccb6444416a4436d32
J Chris Kaltwasser <chris@chrisk.com>
2022-12-29 16:54:28 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { CreateNote, NavBar, NoteUICollection, UpdateNote } from './ui-components'
+
+function App() {
+  return (
+    <>
+      <NavBar width="100%" marginBottom='20px' />
+      <div className='container'>
+        <NoteUICollection />
+      </div>
+      <div className='modal' style={{display: 'none'}}>
+        <CreateNote />
+      </div>
+      <div className='modal' style={{display: 'none'}}>
+        <UpdateNote />
+      </div>
+    </>
+  )
+}
+
+
+export default App;
