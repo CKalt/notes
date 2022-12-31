3a4ebc492274c3bf0c778dac500e23057366ca47
J Chris Kaltwasser <chris@chrisk.com>
2022-12-31 16:05:40 -0500

    added state vars and mutation functions and overrides for showing create note form.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import { CreateNote, NavBar, NoteUICollection, UpdateNote } from './ui-components'
+import { useState } from 'react'
 
-function App() {
+function App () {
+  const [showCreateModal, setShowCreateModal] = useState(false)
+  const [showUpdateModal, setShowUpdateModal] = useState(false)
+  const [updateNote, setUpdateNote] = useState()
+
   return (
     <>
-      <NavBar width="100%" marginBottom='20px' />
+      <NavBar
+        width="100%" marginBottom='20px' overrides={{
+            Button31632483: { onClick: () => setShowCreateModal(true) }
+        }}
+      />
       <div className='container'>
         <NoteUICollection />
       </div>
-      <div className='modal' style={{display: 'none'}}>
-        <CreateNote />
+      <div className='modal' style={{ display: showCreateModal === false && 'none' }}>
+        <CreateNote overrides={{
+            MyIcon: {
+              as: 'button',
+              onClick: () => setShowCreateModal(false)
+            }
+          }}
+        />
       </div>
+
       <div className='modal' style={{display: 'none'}}>
         <UpdateNote />
       </div>
